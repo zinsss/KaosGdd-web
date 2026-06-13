a875bc0b35606f5dffbf7a2e2349b5a8b5e29811
zinsss <87020914+zinsss@users.noreply.github.com>
2026-06-13 16:02:38 +0900

    Add Family done task route

---
+++ frontend/app/family/tasks/done/page.js
@@ -0,0 +1,9 @@
+import FamilyDoneTasksClient from "./FamilyDoneTasksClient";
+
+export const metadata = {
+  title: "완료한 할 일 - KaosGdd",
+};
+
+export default function FamilyDoneTasksPage() {
+  return <FamilyDoneTasksClient />;
+}
