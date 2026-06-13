fc01ca5c4dc83225413a1b6cdc65af7ae0daff4f
zinsss <87020914+zinsss@users.noreply.github.com>
2026-06-13 16:02:36 +0900

    Add Family new task route

---
+++ frontend/app/family/tasks/new/page.js
@@ -0,0 +1,9 @@
+import FamilyTaskFormClient from "../FamilyTaskFormClient";
+
+export const metadata = {
+  title: "할 일 추가 - KaosGdd",
+};
+
+export default function FamilyNewTaskPage() {
+  return <FamilyTaskFormClient />;
+}
