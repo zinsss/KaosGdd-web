f4e0c0e3b9bc4fb1f3a21c7d6b4d898cb06e2d52
zinsss <87020914+zinsss@users.noreply.github.com>
2026-06-14 19:46:41 +0900

    Add family Roni route

---
+++ frontend/app/family/calendar/roni/page.js
@@ -0,0 +1,9 @@
+import FamilyRoniClient from "./FamilyRoniClient";
+
+export const metadata = {
+  title: "로니 - KaosGdd",
+};
+
+export default function FamilyRoniPage() {
+  return <FamilyRoniClient />;
+}
