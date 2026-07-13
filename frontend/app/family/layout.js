c2040307daa09af8690beb7c77dc5af01af22482
zinsss <87020914+zinsss@users.noreply.github.com>
2026-07-13 21:10:13 +0900

    Use Family icon for Family routes

---
+++ frontend/app/family/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  icons: {
+    icon: "/family-icon.svg",
+    shortcut: "/family-icon.svg",
+    apple: "/family-icon.svg",
+  },
+};
+
+export default function FamilyLayout({ children }) {
+  return children;
+}
